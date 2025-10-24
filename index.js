fc002e585ccbeb63184ea1b9fce590a8a1f90f98
Manolo Sainas <sainas.manolo@gmail.com>
2025-10-24 13:55:56 +0200

    Aggiunto prettier, aggiunta navbar con logo

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
-import express from "express"
-import bodyParser from "body-parser"
+import express from 'express';
+import bodyParser from 'body-parser';
 
-const app = express()
-const port = 3000
-const posts = []
+const app = express();
+const port = 3000;
+const posts = [];
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", {posts})
-})
-
-app.get("/editPost", (req, res) => {
-    const index = req.query.index; 
-    res.render("editPost.ejs", {posts, index})
-})
-
-app.post("/submit", (req, res) => {
-    const title = req.body.title.trim()
-    const body = req.body.blogPost.trim()
-    if(title && body){
-           posts.push({ title, body });
-    }
-    res.redirect("/");
-})
-
-app.post("/editPost", (req, res) => {
-    const index = req.body.index;
-    if (posts[index]) {
-        posts[index].title = req.body.title.trim();
-        posts[index].body = req.body.blogPost.trim();
-    }
-    res.redirect("/");
+app.get('/', (req, res) => {
+  res.render('index.ejs', { posts });
 });
 
-app.post("/deletePost", (req, res) => {
-    const index = req.body.index;
-    posts.splice(index, 1);
-    res.redirect("/");
-})
+app.get('/editPost', (req, res) => {
+  const index = req.query.index;
+  res.render('editPost.ejs', { posts, index });
+});
 
-app.listen(port, () => 
-{
-    console.log(`Port number: ${port}`)
-})
+app.post('/submit', (req, res) => {
+  const title = req.body.title.trim();
+  const body = req.body.blogPost.trim();
+  if (title && body) {
+    posts.push({ title, body });
+  }
+  res.redirect('/');
+});
+
+app.post('/editPost', (req, res) => {
+  const index = req.body.index;
+  if (posts[index]) {
+    posts[index].title = req.body.title.trim();
+    posts[index].body = req.body.blogPost.trim();
+  }
+  res.redirect('/');
+});
+
+app.post('/deletePost', (req, res) => {
+  const index = req.body.index;
+  posts.splice(index, 1);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Port number: ${port}`);
+});
