1bbab0f634d8a07d90dff8d6c285182bcd3d0c43
Manolo Sainas <sainas.manolo@gmail.com>
2025-11-03 10:57:45 +0100

    Aggiunti i bottoni per spostarsi tra i vari post

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 const posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/bootstrap-icons', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { posts });
